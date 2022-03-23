adce7116622ce74852be6c13c05dce7f5926ddd9
王立梦 <wanglimeng9>
2022-03-23 09:52:49 +0800

    fix

--- dailySystem/views.py
+++ dailySystem/views.py
@@ -227,7 +227,10 @@ def get_all_result(request):
     # return Response(dict1)
 
 ###tanassddssd999999
-##########测试测试
+##########测试测试####
+###tanassddssd999999
+##########测试测试#### ###tanassddssd999999
+##########测试测试####
 
 if __name__ == '__main__':
     print(get_all_result())
